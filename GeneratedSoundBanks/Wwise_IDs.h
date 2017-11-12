e95210e6d65bca79826db9208569ef2b3b7d9c83
Master Of Evil <guillaume.duquesne@outlook.fr>
2017-11-12 10:38:59 -0500

    qqwq

---
+++ GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,69 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMBIANCE = 3174479978U;
+        static const AkUniqueID PLAY_AXE_HIT = 1776375684U;
+        static const AkUniqueID PLAY_AXE_MISS = 1986109701U;
+        static const AkUniqueID PLAY_BATTLEMUSIC = 3926520445U;
+        static const AkUniqueID PLAY_CROWD_DECEPTION = 3293666249U;
+        static const AkUniqueID PLAY_CROWD_EXTATIC = 2618475426U;
+        static const AkUniqueID PLAY_CROWD_GETHYPED = 2757160520U;
+        static const AkUniqueID PLAY_CROWD_SURPRISE = 2846880533U;
+        static const AkUniqueID PLAY_FLAIL_HIT = 1413915552U;
+        static const AkUniqueID PLAY_FLAIL_MISS = 1092160905U;
+        static const AkUniqueID PLAY_HELM_DOWN = 1605983305U;
+        static const AkUniqueID PLAY_HORSE_ROCKING = 84185841U;
+        static const AkUniqueID PLAY_JINGLE_ALLMISS = 3223684645U;
+        static const AkUniqueID PLAY_JINGLE_START = 2606865948U;
+        static const AkUniqueID PLAY_JINGLE_SUCCESS = 2513009785U;
+        static const AkUniqueID PLAY_LANCE_HIT = 3574353209U;
+        static const AkUniqueID PLAY_LANCE_MISS = 2977442550U;
+        static const AkUniqueID PLAY_PLAYERKILLEDTOOSOON = 2030803885U;
+        static const AkUniqueID PLAY_WEAPON_SELECTED = 1234332190U;
+        static const AkUniqueID STOP_AMBIANCE = 2839405212U;
+        static const AkUniqueID STOP_BATTLEMUSIC = 3329728603U;
+        static const AkUniqueID STOP_CROWDS = 2230898146U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace ENDFIGHT
+        {
+            static const AkUniqueID GROUP = 1263061326U;
+
+            namespace STATE
+            {
+                static const AkUniqueID END = 529726532U;
+                static const AkUniqueID FIGHT = 514064485U;
+            } // namespace STATE
+        } // namespace ENDFIGHT
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK = 1661994096U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
